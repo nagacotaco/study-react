6ea71f325e80891cd999e16d61163214c9557e4e
NuggetDyson <nagacotaco@gmail.com>
2023-11-11 22:01:02 +0900

    ♻️ Linksをリファクタ

--- components/Links.tsx
+++ components/Links.tsx
@@ -1,66 +1,51 @@
-import { Inter } from 'next/font/google'
-import styles from './Links.module.css'
+import styles from './Links.module.css';
 
-const inter = Inter({ subsets: ['latin'] })
+const ITEMS = [
+  {
+    href: 'https://www.scsk-kyushu.co.jp/recruit/interview/16.html',
+    title: 'SCSK',
+    description:
+      'I joined this company as a new graduate. I did not enjoy my job. It was hard.'
+  },
+  {
+    href: 'https://www.tetrabox.tech/',
+    title: 'Tetrabox.inc',
+    description:
+      'Established on 3/31/2023. I enjoy working with my two colleagues!'
+  },
+  {
+    href: 'https://billage.space/members/tetrabox',
+    title: 'Billage',
+    description:
+      'Featured in an article about a shared office currently in use.'
+  },
+  {
+    href: 'https://runrunrun.jp/wp/wp-content/uploads/2023/02/%E3%83%8F%E3%83%BC%E3%83%95%E3%81%BF%E3%81%A9%E3%82%8A%E4%BC%9A%E7%94%B7%E5%AD%90-1.pdf',
+    title: 'Marathon Result',
+    description:
+      'I ran a marathon with three work colleagues. The results are at\n.....See the link.'
+  }
+];
 
 export function Links() {
   return (
     <div className={styles.grid}>
-      <a
-        href="https://www.scsk-kyushu.co.jp/recruit/interview/16.html"
-        className={styles.card}
-        target="_blank"
-        rel="noopener noreferrer"
-      >
-        <h2 className={styles.title}>
-          SCSK <span>-&gt;</span>
-        </h2>
-        <p className={styles.description}>
-          I joined this company as a new graduate. I did not enjoy my job. It was hard.
-        </p>
-      </a>
-
-      <a
-        href="https://www.tetrabox.tech/"
-        className={styles.card}
-        target="_blank"
-        rel="noopener noreferrer"
-      >
-        <h2 className={styles.title}>
-          Tetrabox.inc <span>-&gt;</span>
-        </h2>
-        <p className={styles.description}>
-          Established on 3/31/2023. I enjoy working with my two colleagues!
-        </p>
-      </a>
-
-      <a
-        href="https://billage.space/members/tetrabox"
-        className={styles.card}
-        target="_blank"
-        rel="noopener noreferrer"
-      >
-        <h2 className={styles.title}>
-          Billage Article <span>-&gt;</span>
-        </h2>
-        <p className={styles.description}>
-          Featured in an article about a shared office currently in use.
-        </p>
-      </a>
-
-      <a
-        href="chrome-extension://oemmndcbldboiebfnladdacbdfmadadm/https://runrunrun.jp/wp/wp-content/uploads/2023/02/%E3%83%8F%E3%83%BC%E3%83%95%E3%81%BF%E3%81%A9%E3%82%8A%E4%BC%9A%E7%94%B7%E5%AD%90-1.pdf"
-        className={styles.card}
-        target="_blank"
-        rel="noopener noreferrer"
-      >
-        <h2 className={styles.title}>
-          Marathon Result <span>-&gt;</span>
-        </h2>
-        <p className={styles.description}>
-          I ran a marathon with three work colleagues. The results are at ..... See the link.
-        </p>
-      </a>
+      {ITEMS.map((item) => {
+        return (
+          <a
+            key={item.href}
+            href={item.href}
+            className={styles.card}
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            <h2 className={styles.title}>
+              {item.title} <span>-&gt;</span>
+            </h2>
+            <p className={styles.description}>{item.description}</p>
+          </a>
+        );
+      })}
     </div>
-  )
+  );
 }
