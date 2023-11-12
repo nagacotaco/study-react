16fef53f443c6901ee6f3c2da1e1af3d3be547f6
NuggetDyson <nagacotaco@gmail.com>
2023-11-12 13:34:48 +0900

    ⚗️ 不要な記述でいつか消しますが、試しにクリックイベントを実装しました

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,15 +1,22 @@
 import Head from 'next/head';
+import { useCallback } from 'react';
 import { Header } from 'src/components/Header/Header';
 import { Main } from 'src/components/Main/Main';
 import styles from 'src/styles/Home.module.css';
 
 export default function Home() {
+  const handleClick = useCallback((e: any) => {
+    e.preventDefault();
+  }, []);
   return (
     <div className={styles.container}>
       <Head>
         <title>Index Page</title>
       </Head>
       <Header />
+      <a href="/about" onClick={handleClick}>
+        ボタン
+      </a>
       <Main page="index"></Main>
     </div>
   );
