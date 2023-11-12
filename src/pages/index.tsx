6f20074eaf7114cf85e6bde4a1c5da66fa0f08bc
NuggetDyson <nagacotaco@gmail.com>
2023-11-12 16:27:10 +0900

    ✨ カウンターを設置してuseStateで状態管理

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,30 +1,33 @@
 import Head from 'next/head';
-import Link from 'next/link';
-import { useCallback, useEffect } from 'react';
+import { useEffect, useState } from 'react';
 import { Header } from 'src/components/Header/Header';
 import { Main } from 'src/components/Main/Main';
 import styles from 'src/styles/Home.module.css';
 
 export default function Home() {
+  const [count, setCount] = useState(1);
+
   useEffect(() => {
     document.body.style.backgroundColor = 'lightblue';
     return () => {
       document.body.style.backgroundColor = '';
     };
   }, []);
-  const handleClick = useCallback((e: any) => {
-    e.preventDefault();
-    alert('123');
-  }, []);
+
+  const handleClick = (e: any) => {
+    setCount((count) => count + 1);
+  };
+
   return (
     <div className={styles.container}>
       <Head>
         <title>Index Page</title>
       </Head>
       <Header />
-      <Link href="/about" onClick={handleClick}>
-        ボタン
-      </Link>
+      <div>
+        <h1>{count}</h1>
+        <button onClick={handleClick}>ボタン</button>
+      </div>
       <Main page="index"></Main>
     </div>
   );
